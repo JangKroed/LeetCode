28bb6bf1839c1f92905a64f4e55a6d622558ece0
Jang Yong Ho <96761280+JangKroed@users.noreply.github.com>
2023-08-23 22:51:55 +0900

    Time: 69 ms (72.70%), Space: 45.3 MB (59.34%) - LeetHub

--- 0026-remove-duplicates-from-sorted-array/0026-remove-duplicates-from-sorted-array.ts
+++ 0026-remove-duplicates-from-sorted-array/0026-remove-duplicates-from-sorted-array.ts
@@ -1,11 +1,14 @@
 function removeDuplicates(nums: number[]): number {
-  const arr = [...new Set(nums)];
+  let prev: number = nums[0];
+  let count: number = 1;
 
-  for (let i = 0; i < nums.length; i++) {
-    if (i < arr.length) {
-      nums[i] = arr[i];
+  for (let i = 1; i < nums.length; i++) {
+    if (prev !== nums[i]) {
+      nums[count] = nums[i];
+      count++;
+      prev = nums[i];
     }
   }
 
-  return arr.length;
+  return count;
 }
