cae37d6298ffe2ef1a4ada8506435e3ce5f70d73
Jang Yong Ho <96761280+JangKroed@users.noreply.github.com>
2023-08-27 12:39:58 +0900

    Time: 58 ms (95.07%), Space: 46.3 MB (57.17%) - LeetHub

--- 0125-valid-palindrome/0125-valid-palindrome.ts
+++ 0125-valid-palindrome/0125-valid-palindrome.ts
@@ -1,63 +1,19 @@
 function isPalindrome(s: string): boolean {
-  if (s.trim().length === 0) {
-    return true;
-  }
-
-  const alphanumericArray: string[] = [
-    "0",
-    "1",
-    "2",
-    "3",
-    "4",
-    "5",
-    "6",
-    "7",
-    "8",
-    "9",
-    "a",
-    "b",
-    "c",
-    "d",
-    "e",
-    "f",
-    "g",
-    "h",
-    "i",
-    "j",
-    "k",
-    "l",
-    "m",
-    "n",
-    "o",
-    "p",
-    "q",
-    "r",
-    "s",
-    "t",
-    "u",
-    "v",
-    "w",
-    "x",
-    "y",
-    "z",
-  ];
-
-  let temp: string = "";
-
-  for (const str of s) {
-    if (alphanumericArray.includes(str.toLowerCase())) {
-      temp += str.toLowerCase();
-    }
-  }
+  let temp: string = s.replace(/[^a-zA-Z0-9]/g, "").toLowerCase();
 
   const isEvenNum = temp.length % 2 === 0 ? 0 : 1;
 
   const start = temp.slice(0, Math.floor(temp.length / 2));
-  const end = temp
-    .slice(Math.floor(temp.length / 2 + isEvenNum), temp.length)
-    .split("")
-    .reverse()
-    .join("");
+  const end = temp.slice(Math.floor(temp.length / 2 + isEvenNum), temp.length);
 
-  return start === end;
+  let endIdx = end.length - 1;
+
+  for (const str of start) {
+    if (str !== end[endIdx]) {
+      return false;
+    }
+    endIdx--;
+  }
+
+  return true;
 }
