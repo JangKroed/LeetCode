1c96d4a812d06116ac3ee9896ee9932328b7683b
Jang Yong Ho <96761280+JangKroed@users.noreply.github.com>
2023-08-31 22:12:22 +0900

    Time: 102 ms (33.26%), Space: 44.7 MB (64.24%) - LeetHub

--- 0001-two-sum/0001-two-sum.ts
+++ 0001-two-sum/0001-two-sum.ts
@@ -2,7 +2,7 @@ function twoSum(nums: number[], target: number): number[] {
   let result: number[] = [];
 
   for (let i = 0; i < nums.length; i++) {
-    for (let j = 0; j < nums.length; j++) {
+    for (let j = i + 1; j < nums.length; j++) {
       if (i === j) {
         continue;
       }
@@ -13,5 +13,5 @@ function twoSum(nums: number[], target: number): number[] {
     }
   }
 
-  return result.slice(0, 2);
+  return result;
 }
